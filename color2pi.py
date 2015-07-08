8934c8cb402b5a9162a9faf28c626d25d303b6fc
5shekel <yair99@gmail.com>
2015-07-08 13:38:49 +0000

    working serial comm and color avrage

---
+++ color2pi.py
@@ -0,0 +1,34 @@
+import io
+from PIL import Image
+from PIL import ImageStat
+
+import picamera 
+import serial
+from time import sleep
+
+ser=serial.Serial('/dev/ttyAMA0',9600)
+newline='\n'
+
+
+
+while 1:
+	with picamera.PiCamera() as camera:
+		sleep(0.2)
+		
+		camera.resolution = (80,60)
+		camera.brightness=50
+		camera.saturation=90
+		#camera.start_preview()
+		camera.capture('image.png',resize=(30,20))
+
+	im=Image.open('image.png')
+	left=10
+	top=7
+	im2 = im.crop((left,top,left+5,top+5))
+	im2.save("temp"+".png")
+	avg =  ImageStat.Stat(im2).median
+	print(avg)
+	r,g,b,s=avg
+	mesg=str(r)+','+str(g)+','+str(b)+newline
+	ser.write(mesg)
+
