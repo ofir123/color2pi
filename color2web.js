13eab7c691b7251fd4153fbccc2f950616f3cd99
yair reshef <yair99@gmail.com>
2015-07-11 06:05:43 +0300

    js triming

--- color2web.js
+++ color2web.js
@@ -4,11 +4,10 @@
 
 //color sensor to  webpage background
 
-var zmq = require('zmq');
-// socket to talk to clients
-var responder = zmq.socket('rep');
 
 
+///////////  servi function  ////////////
+///////////////////////////////////////////
 var latestData = 0;
 var servi = require('servi');
 var app = new servi(false); // servi instance
@@ -20,20 +19,30 @@ app.route('/data', sendData); // route requests for /data to sendData()
 // now that everything is configured, start the server:
 app.start();
 
-responder.on('message', function(request) {
-  console.log("Received request: [", request.toString(), "]");
+function sendData(ServiRequest) {
+  // print  that a client HTTP request came in to the server:
+  console.log("Got a client request, sending them the data.");
+  // respond to the client request with the latest serial string:
+  ServiRequest.respond(latestData);
+}
 
-  // do some 'work'
+//////// ZMQ ////////
+var zmq = require('zmq');
+// socket to talk to clients
+var zmqResponder = zmq.socket('rep');
+zmqResponder.on('message', function(request) {
+  console.log("Received request: [", request.toString(), "]");
+  request.toString();
+  /* do some 'work'
   setTimeout(function() {
 
     // send reply back to client.
-    responder.send("OK");
+    zmqResponder.send("OK");
   }, 1000);
+  */
 });
 
-
-// ZMQ funcs
-responder.bind('tcp://*:5556', function(err) {
+zmqResponder.bind('tcp://*:5556', function(err) {
   if (err) {
     console.log(err);
   } else {
@@ -42,31 +51,11 @@ responder.bind('tcp://*:5556', function(err) {
 });
 
 process.on('SIGINT', function() {
-  responder.close();
+  zmqResponder.close();
 });
-
-
-
 //used to move things, back when we worked with serial
+//serialPort.on('data', saveLatestData);
 function saveLatestData(data) {
-   //console.log(data);
+   console.log(data);
    latestData = data;
 }
-function sendData(request) {
-  // print out the fact that a client HTTP request came in to the server:
-  console.log("Got a client request, sending them the data.");
-  // respond to the client request with the latest serial string:
-  request.respond(latestData);
-}
-
-function showData(result) {
-  var resultString = result[0];
-  text.html("Sensor reading:" + resultString);
-  // split it:
-  var numbers = split(resultString, " ");
-  // use the numbers:
-  text.position(numbers[0], numbers[1]);
-  text.style("font-size", numbers[2] + "%");
- }
-
-//serialPort.on('data', saveLatestData);
