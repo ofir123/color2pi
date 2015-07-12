172f43adb6045eb029edafbc20e173065857da3a
Ofir <ofirbrukner@gmail.com>
2015-07-13 00:28:28 +0300

    Added polling logic.

--- color2pi.py
+++ color2pi.py
@@ -14,6 +14,8 @@ OK_REPLY = 'OK'
 COLORS_DELIMITER = ','
 
 SLEEP_PERIOD = 0.2
+# Socket timeout (in milliseconds).
+SOCKET_TIMEOUT = 3000
 
 log = logbook.Logger('Color2Pi')
 
@@ -35,7 +37,12 @@ def main():
 	context = zmq.Context()
 	log.info('Connecting to server ({0})...'.format(server_address))
 	socket = context.socket(zmq.REQ)
+	# Important for the close operation.
+	socket.setsockopt(zmq.LINGER, 0)
 	socket.connect ('tcp://{0}'.format(server_address))
+	# Create a poller for socket timeouts.
+	poller = zmq.Poller()
+	poller.register(socket, zmq.POLLIN)
 	# Set up serial port.
 	ser = serial.Serial('/dev/ttyAMA0',9600)
 	# Start working!
@@ -56,25 +63,32 @@ def main():
 			avg = ImageStat.Stat(im2).median
 			r, g, b, s = avg
 			message = '{0}{1}{2}{3}{4}'.format(r, COLORS_DELIMITER, g, COLORS_DELIMITER, b)
+			# Send to serial.
 			ser.write(message + '\n')
 			log.info('Colors are: {0}'.format(message))
-			# Send to server.
-			socket.send(message)
-			# Get the reply.
-			message = socket.recv()
-			if message != OK_REPLY:
-				log.info('Got an unknown message: {0}. Stopping...'.format(message))
-				socket.send(STOP_MESSAGE)
-				break
-			# Send to serial.
+			if socket is not None:
+				# Send to server.
+				socket.send(message)
+				# Get the reply (with a SOCKET_TIMEOUT timeout).			
+				if poller.poll(SOCKET_TIMEOUT):
+					message = socket.recv()
+					if message != OK_REPLY:
+						log.info('Got an unknown message: {0}. Stopping...'.format(message))
+						socket.send(STOP_MESSAGE)
+						break
+				else:
+					# Stop using the socket, since there's no response from the other side.
+					socket.close()
+					socket = None
 	except KeyboardInterrupt:
-		try:
-			# Tell the server to stop as well.
-			socket.send(STOP_MESSAGE)
-		except zmq.core.error.ZMQError:
-			# Wrong state, we have to receive the OK message first.
-			socket.recv()
-			socket.send(STOP_MESSAGE)
+		if socket is not None:
+			try:
+				# Tell the server to stop as well.
+				socket.send(STOP_MESSAGE)
+			except zmq.core.error.ZMQError:
+				# Wrong state, we have to receive the OK message first.
+				socket.recv()
+				socket.send(STOP_MESSAGE)
 	
 
 if __name__ == '__main__':
