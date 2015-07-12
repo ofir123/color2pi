318c230e6868922fc19d94817fc8f7703f8ee537
5shekel <yair99@gmail.com>
2015-07-12 14:23:25 +0000

    ad scrpit to send ip on boot

---
+++ ip-on-boot.py
@@ -0,0 +1,62 @@
+#see http://elinux.org/RPi_Email_IP_On_Boot_Debian
+# add to /etc/rc.local in if
+# python ip-on-boot.py 'password'
+
+__author__ = 'Cody Giles'
+__license__ = "Creative Commons Attribution-ShareAlike 3.0 Unported License"
+__version__ = "1.0"
+__maintainer__ = "Cody Giles"
+__status__ = "Production"
+
+import sys
+import subprocess
+import smtplib
+from email.mime.text import MIMEText
+import datetime
+
+if len(sys.argv) > 1:
+    gmail_password=sys.argv[1]
+
+def connect_type(word_list):
+    """ This function takes a list of words, then, depeding which key word, returns the corresponding
+    internet connection type as a string. ie) 'ethernet'.
+    """
+    if 'wlan0' in word_list:
+        con_type = 'wifi'
+    elif 'eth0' in word_list:
+        con_type = 'ethernet'
+    else:
+        con_type = 'current'
+
+    return con_type
+
+# Change to your own account information
+# Account Information
+to = 'yair99@gmail.com' # Email to send to.
+gmail_user = 'shenkarse03@gmail.com' # Email to send from. (MUST BE GMAIL)
+#gmail_password = 'password' # Gmail password.
+smtpserver = smtplib.SMTP('smtp.gmail.com', 587) # Server to use.
+
+smtpserver.ehlo()  # Says 'hello' to the server
+smtpserver.starttls()  # Start TLS encryption
+smtpserver.ehlo()
+smtpserver.login(gmail_user, gmail_password)  # Log in to server
+today = datetime.date.today()  # Get current time/date
+
+arg='ip route list'  # Linux command to retrieve ip addresses.
+# Runs 'arg' in a 'hidden terminal'.
+p=subprocess.Popen(arg,shell=True,stdout=subprocess.PIPE)
+data = p.communicate()  # Get data from 'p terminal'.
+
+# Split IP text block into three, and divide the two containing IPs into words.
+ip_lines = data[0].splitlines()
+
+# Creates the text, subject, 'from', and 'to' of the message.
+msg = MIMEText(str(ip_lines).strip('[]'))
+msg['Subject'] = 'IPs For RaspberryPi on %s' % today.strftime('%b %d %Y')
+msg['From'] = gmail_user
+msg['To'] = to
+# Sends the message
+smtpserver.sendmail(gmail_user, [to], msg.as_string())
+# Closes the smtp server.
+smtpserver.quit()
